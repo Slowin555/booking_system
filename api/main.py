015c7eec226e20c0c3bfb7c2764c66188d91c214
Slowin555 <alex0508smol@gmail.com>
2025-10-13 23:55:26 +0300

    Просто тест

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import os
-
+"Просто текст ради теста фетча"
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup
